3cbb1207fec1ea88e8657a64bc0c81cab6333c09
Feraldar1303l <bocanegrafer04@gmail.com>
2021-01-22 14:11:28 -0600

    primer commit

---
+++ bin/get_set.dart
@@ -0,0 +1,15 @@
+import 'persona.dart';
+import 'triangulo.dart';
+
+void main(List<String> arguments) {
+  /*Triangulo triangulo = new Triangulo();
+
+  triangulo.base = 7.5;
+  triangulo.altura = 23.5;
+
+  print('El area del triangulo es: ${triangulo.area}');
+  */
+  Persona p = new Persona();
+  p.nombre = 'Fernando';
+  print('El nombre es: ${p.nombrePersona}');
+}
